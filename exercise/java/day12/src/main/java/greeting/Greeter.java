010bdf7d670de31d9ec598122cf1394846ee46b7
Emile RICAUD <emile.ricaud@gmail.com>
2024-01-28 23:04:37 +0100

    Finish Exercise 12 (Open to extension : Map solution)

--- exercise/java/day12/src/main/java/greeting/Greeter.java
+++ exercise/java/day12/src/main/java/greeting/Greeter.java
@@ -1,22 +1,33 @@
 package greeting;
 
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Optional;
+
 public class Greeter {
     String formality;
+
+    public enum Formality {
+        formal, casual, intimate;
+    }
+
+    private static final Map<Formality, String> mapping;
+
+    static {
+        mapping = new LinkedHashMap<>();
+        mapping.put(Formality.formal, "Good evening, sir.");
+        mapping.put(Formality.casual, "Sup bro?");
+        mapping.put(Formality.intimate, "Hello Darling!");
+    }
 
     public String greet() {
         if (this.formality == null) {
             return "Hello.";
         }
 
-        if (this.formality.equals("formal")) {
-            return "Good evening, sir.";
-        } else if (this.formality.equals("casual")) {
-            return "Sup bro?";
-        } else if (this.formality.equals("intimate")) {
-            return "Hello Darling!";
-        } else {
-            return "Hello.";
-        }
+        return Optional.ofNullable(mapping.get(Formality.valueOf(formality))).orElse("Hello");
+
     }
 
     public void setFormality(String formality) {
