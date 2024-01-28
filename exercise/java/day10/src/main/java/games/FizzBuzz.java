b97fbb2bea68ebd3229e4961e87ce659c9a44a71
Emile RICAUD <emile.ricaud@gmail.com>
2024-01-28 22:15:58 +0100

    Finish Exercise 10 (Functional Programming) : Set on a map key(predicate with test) and value (function to convert input to string) With a stream, filter keys who the predicate return true with input, and convert with value function. if Empty -> OutOfBound

--- exercise/java/day10/src/main/java/games/FizzBuzz.java
+++ exercise/java/day10/src/main/java/games/FizzBuzz.java
@@ -1,5 +1,10 @@
 package games;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
 public class FizzBuzz {
     public static final int MIN = 0;
     public static final int MAX = 100;
@@ -7,27 +12,35 @@ public class FizzBuzz {
     public static final int BUZZ = 5;
     public static final int FIZZBUZZ = 15;
 
+    public static final boolean DEFAULT = true;
+
+    private static final Map<Predicate<Integer>, Function<Integer, String>> mapping;
+
+    static {
+        mapping = new LinkedHashMap<>();
+        mapping.put(i -> is(FIZZBUZZ, i), i -> "FizzBuzz");
+        mapping.put(i -> is(FIZZ, i), i -> "Fizz");
+        mapping.put(i -> is(BUZZ, i), i -> "Buzz");
+        // Default value for numbers in range
+        mapping.put(i -> DEFAULT, Object::toString);
+    }
+
     private FizzBuzz() {
     }
 
     public static String convert(Integer input) throws OutOfRangeException {
-        if (isOutOfRange(input)) {
-            throw new OutOfRangeException();
-        }
-        return convertSafely(input);
-    }
-
-    private static String convertSafely(Integer input) {
-        if (is(FIZZBUZZ, input)) {
-            return "FizzBuzz";
-        }
-        if (is(FIZZ, input)) {
-            return "Fizz";
-        }
-        if (is(BUZZ, input)) {
-            return "Buzz";
-        }
-        return input.toString();
+        return mapping.entrySet()
+                .stream()
+                // use filter function to check if the input is valid
+                .filter(f -> !isOutOfRange(input))
+                // We start by filtering the stream based on wether the Predicate is matching the value or not
+                .filter(f -> f.getKey().test(input))
+                // We keep only the first entry
+                .findFirst()
+                // We take the associated value (Function<Integer, String>)
+                .map(v -> v.getValue().apply(input))
+                // We throw the Exception if no match
+                .orElseThrow(OutOfRangeException::new);
     }
 
     private static boolean is(Integer divisor, Integer input) {
